2abe7b0587fd41473368755ac8c7a719ecb514a8
Yancarlo Urdaneta <yancarlo.urdaneta@airtek.com.ve>
2022-01-06 19:30:01 -0400

    add color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   mode: "jit",
   content: ["./src/**/*.{js,jsx,ts,tsx}","./public/index.html"],
   theme: {
+    colors: {
+      ...colors,
+      'main': '#302138',
+      'secondary': '#422D4D',
+      'main-button': '#5E00D4',
+      'white': '#E5E5E5'
+    },
     extend: {},
   },
   plugins: [],
