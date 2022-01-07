1b42d8dee73f41b9d8e08fe2beac6b6f790df302
Yancarlo Urdaneta <yancarlo.urdaneta@airtek.com.ve>
2022-01-07 02:54:23 -0400

    change mt to pt

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import HEADER from "../interfaces/header"
 const Header = ({title}:HEADER) => {
   return (
     <h1 
-      className="mt-20 text-3xl text-white">
+      className="pt-20 md:pt-0 text-3xl text-white">
         {title}
     </h1>)
 }
