ec4f17f393ca562bab8b0a0debf90d8eaa257ca7
Yancarlo Urdaneta <yancarlo.urdaneta@airtek.com.ve>
2022-01-29 11:36:28 -0400

    add styles prop and font bold

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,9 @@ import HEADER from "../interfaces/header"
 const Header = ({title, styles}:HEADER) => {
   return (
     <h1 
-      className={`pt-20 md:pt-0 text-3xl text-white ${styles}`}>
+      className={
+        `pt-20 md:pt-0 text-3xl text-white font-bold 
+        ${styles}`}>
         {title}
     </h1>)
 }
