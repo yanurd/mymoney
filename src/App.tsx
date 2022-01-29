c64204a4fa36350b8d230e04144da73f1b1d98a8
Yancarlo Urdaneta <yancarlo.urdaneta@airtek.com.ve>
2022-01-29 11:35:15 -0400

    routing app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import Register from "./containers/Register"
+import { lazy, Suspense } from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import Login from "./pages/Login"
+const Register = lazy(() => import("./pages/Register"))
 
 const App = () => {
   return (
     <section className="bg-main min-h-screen flex flex-col justify-center items-center" >
-      <Register/>
+    <BrowserRouter>
+      <Routes>
+        <Route path='' element={<Login />} />
+        <Route path='/register' element={
+          <Suspense fallback={<>...</>} >
+            <Register />
+          </Suspense>} />
+      </Routes>
+    </BrowserRouter>
     </section>
     
   )
