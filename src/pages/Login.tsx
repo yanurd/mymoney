4dcf304332a84d4ba54aa29618b5ecc3c0d03f4f
Yancarlo Urdaneta <yancarlo.urdaneta@airtek.com.ve>
2022-01-30 18:13:03 -0400

    manage errors in form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -12,47 +12,54 @@ import { LoginForm } from "../interfaces/login"
 
 
 const Login = () => {
-  const { register, handleSubmit } = useForm<LoginForm>()
-  const onSubmit = (data: LoginForm) => console.log(data)
+  const { register, handleSubmit, watch, formState: {errors} } = useForm<LoginForm>()
+  const loginForm = (data: LoginForm) => console.log(data)
+  
   return (
     <section className="mx-14 flex flex-col justify-center items-center">
       <Header title="My Money" styles="mb-12"/>
 
       <form 
-        onSubmit={handleSubmit(onSubmit)}
-        className="mt-8 flex flex-col ">
-      <h2 className="text-white">Welcome Back</h2>
-        <label className="my-3">
+        onSubmit={handleSubmit(loginForm)}
+        className="mt-8 flex flex-col text-white ">
+      <h2 className="text-white font-bold text-lg">Welcome Back</h2>
+        <label className="my-3 flex flex-col">
+          <span>Email</span>
           <input
-            required
             placeholder="Enter your mail address"
-            className="pl-4 p-2 bg-secondary rounded text-white" 
-            {...register('email'), 
-            {
+            className="pl-4 p-2 bg-secondary rounded " 
+            {...register("email", {
               required: true,
-              message: 'This field is required'
-            },
-            {
               pattern: EmailValidation,
-              message: 'Email format is not correct'
-            }}/>
+            })}/>
+            {
+              errors.email
+              ? <span className="">Invalid email</span>
+              : null
+            }
         </label>
-        <label className="my-3">
+        <label className="my-3 flex flex-col">
+          <span>Password</span>
           <input
             type='password'
             placeholder="Password"
-            className="pl-4 p-2 bg-secondary rounded text-white" 
-            {...register('password'), 
-            {
+            className="pl-4 p-2 bg-secondary rounded " 
+            {...register("password", {
               required: true,
-              message: 'This field is required'
-            }}/>
+              minLength: 8
+            })}
+            />
+            {
+              errors.password 
+                ? <span className="">Password must be longer than 8</span>
+                : null
+            }
         </label>
         <Link to='/password-recovery'
-        className="text-right text-white mb-12 hover:underline hover:underline-offset-8">Forgot password?</Link>
+        className="text-right  mb-12 hover:underline hover:underline-offset-8">Forgot password?</Link>
         <button 
           type="submit"
-          className="py-3 font-bold text-xl text-white bg-main-button rounded cursor-pointer">
+          className="py-3 font-bold text-xl  bg-main-button rounded cursor-pointer">
             Login
           </button>
       </form>
